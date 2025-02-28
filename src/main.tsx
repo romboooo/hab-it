bb950f659950cb1041e12c927430876ab87b3f82
Роман Бурейко <144876262+romboooo@users.noreply.github.com>
2025-02-28 15:30:07 +0300

    update folder name

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import CalendarPage from './pages/master/CalendarPage.tsx'
+import CalendarPage from './pages/Calendar/CalendarPage.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
